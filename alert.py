f729a48143726fdb329a28d0d3f965bf4aafc4f6
Andrea Manzi <andrea.manzi@comune.fi.it>
2019-09-25 14:49:28 +0200

    Start project

--- alert.py
+++ alert.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 import json
 import smtplib
+from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+import datefunction
 
 
 def send(volt):
     with open('config.json') as json_data_file:
         data = json.load(json_data_file)
+
+        lastvolt = data["lastvolt"]
+        lastcheck = datetime.strptime(data["lastalertdate"], '%Y-%m-%d %H:%M:%S')
+        # print(str(float(lastvolt)-float(volt)))
+        if float(lastvolt) <= 0 or (float(lastvolt) > float(volt) and (float(lastvolt) - float(volt) > 0.10)):
+            print(str("Volt " + lastvolt + " -> " + volt))
+        else:
+            if (((datefunction.now().replace(tzinfo=None) - lastcheck).total_seconds()) < 60 * 60):
+                return
 
         smtphost = data["mailserver"]["host"]
         smtpport = data["mailserver"]["port"]
@@ -34,5 +46,9 @@ def send(volt):
             server.sendmail(smtpuser, send_to_email, text)
             server.quit()
             print("Successfully sent email")
+            data["lastalertdate"] = datefunction.nowToDatetimeHrString()
+            data["lastvolt"] = volt
+            with open('config.json', 'w') as outfile:
+                json.dump(data, outfile)
         except:
             print("Error: unable to send email")
